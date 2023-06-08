62d3951cf7f83ed6d2e7a864a2c015ff553c11cd
Sevval Kalkavan <sevvallkalkavannn@gmail.com>
2023-06-08 22:40:55 +0300

    Create IBActions and IBOutlets for the Results Screen

---
+++ Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Şevval Kalkavan on 8.06.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
